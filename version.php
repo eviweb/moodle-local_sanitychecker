8f63297a02a965ba717b644fc059cb8bebaef6eb
Eric Villard <dev@eviweb.fr>
2013-05-16 13:10:51 +0200

    increase version number Signed-off-by:Eric Villard <dev@eviweb.fr>

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
  * @license     http://opensource.org/licenses/MIT MIT License
  */
 
-$plugin->version = 2013041500;
+$plugin->version = 2013051600;
 $plugin->requires = 2012062500;
 $plugin->component = 'local_sanitychecker';
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.2 (Build: '.$plugin->version.')';
+$plugin->release = '0.3 (Build: '.$plugin->version.')';
